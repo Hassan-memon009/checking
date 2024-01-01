92631a0c2ef7453515149087c84fe0828981b913
Muhammad Hassan Memon <134962710+Hassan-memon009@users.noreply.github.com>
2024-01-01 08:55:50 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Header from './Component/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
